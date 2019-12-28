18af5caa372af18f4ee04618cc2f11fdc902b2d1
Kenan Babačić <babacic@bk.ru>
2019-12-28 19:58:32 +0100

    Update database.class.php

--- Class/database.class.php
+++ Class/database.class.php
@@ -1,20 +1,9 @@
 <?php
 
 class Database extends Dbh{
-
-    protected function getAllUsers(){
-        $sql = "SELECT * FROM user";
-        $result = $this->connect()->query($sql);
-        $numRows = $result->num_rows;
-        if ($numRows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $data[] = $row;
-            }
-            return $data;
-        }
-    } 
-    protected function getAllLicense($term){
-        $sql = "SELECT * FROM license WHERE name like '%$term%' || creator like '%$term%' || type like '%$term%'";
+    #Getting username with id
+    protected function getUserById($uid){
+        $sql = "SELECT username FROM user WHERE id = '$uid'";
         $result = $this->connect()->query($sql);
         $numRows = $result->num_rows;
         if ($numRows > 0) {
@@ -24,6 +13,19 @@ class Database extends Dbh{
             return $data;
         }
     }
+    #Getting all license
+    protected function getAllLicense($term){
+        $sql = "SELECT license.id, license.name, license.type, user.username AS creator FROM license LEFT JOIN user ON license.creator = user.id WHERE name like '%$term%' || username like '%$term%' || type like '%$term%'";
+        $result = $this->connect()->query($sql);
+        $numRows = $result->num_rows;
+        if ($numRows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            return $data;
+        }
+    }
+    #Login
     public function loginUser($username, $password){
         $sql = "SELECT * FROM user WHERE username = '$username'";
         $result = $this->connect()->query($sql);
@@ -39,6 +41,7 @@ class Database extends Dbh{
             }
         }
     } 
+    #Delete License
     public function deleteLicense($license_id){
         $sql = "DELETE FROM license WHERE id = '$license_id'";
         $result = $this->connect()->query($sql);
@@ -48,6 +51,7 @@ class Database extends Dbh{
             return false;
         }
     }
+    #Getting license info
     protected function getLicenseInfo($license_id){
         $sql = "SELECT * FROM license WHERE id = '$license_id'";
         $result = $this->connect()->query($sql);
@@ -60,8 +64,9 @@ class Database extends Dbh{
             return false;
         }
     }
-    public function editLicenseInfo($license_id, $name, $type, $period, $creator){
-        $sql = "UPDATE license SET name = '$name', `type` = '$type', `period` = '$period', creator = '$creator' WHERE id = '$license_id'";
+    #Method for license editing
+    public function editLicenseInfo($license_id, $name, $type, $period){
+        $sql = "UPDATE license SET name = '$name', `type` = '$type', `period` = '$period' WHERE id = '$license_id'";
         $result = $this->connect()->query($sql);
         if ($result = true) {
             header("Location: ../index.php");
@@ -69,6 +74,7 @@ class Database extends Dbh{
             header("Location: ../index.php?error=true");
         }
     }
+    #Method for inserting new license
     public function insertLicense($name, $type, $period, $creator){
         $sql = "INSERT INTO license (`name`, `type`, `period`, `creator`) VALUES ('$name', '$type', '$period', '$creator')";
         $result = $this->connect()->query($sql);
